814655faf2a62a5501e4e8c47e65b6bb13131271
SofKard <sofkard@DESKTOP-E7KFGNP.localdomain>
2022-03-29 01:24:49 -0500

    remove submodule

---
+++ ui_2/src/App.js
@@ -0,0 +1,20 @@
+// Sofia Kardonik - sk46637
+import React from "react";
+
+// import navigation bar and css
+import "./App.css";
+
+// import component
+import TextName from "./components/TextName";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <TextName />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ ui_2/src/components/TextName.js
@@ -0,0 +1,37 @@
+import React, {useState} from 'react';
+import TextField from "@mui/material/TextField"
+
+function TextName () {
+    const [first_name, set_first_name] = useState("")
+    const [last_name, set_last_name] = useState("First Name Not Found")
+  
+    return (
+      <div>
+        <div className='input'>
+          <h1>Input here</h1>
+          <label> First name (Sofie) </label>  
+                <TextField value={first_name}
+                  onChange={(e) => {
+                    set_first_name(e.target.value);
+                    //fetch("http://127.0.0.1:5000/input/" + e.target.value)
+                    fetch("/input/" + e.target.value)             // connected to server  
+                    .then(response => response.json())            // setting to json format
+                    .then(data => {set_last_name(data.output)})   // set data to 
+                    .catch(error => {set_last_name(error)})       // catch error if error
+                  }
+                }
+              />          
+        </div>
+
+        <div className='output'>
+          <h1>Output here</h1>
+          <label> From server </label>
+            <TextField variant="outlined" value={last_name} />
+        </div>
+      </div>
+
+    );
+    
+  }
+
+  export default TextName;
