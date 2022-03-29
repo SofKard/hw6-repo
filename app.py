a3f6516cfc4c353a3f1d1506bc76d8d63a7f41ea
SofKard <sofkard@DESKTOP-E7KFGNP.localdomain>
2022-03-29 01:38:10 -0500

    rm print statements and a flask import

--- app.py
+++ app.py
@@ -2,22 +2,16 @@ from flask import Flask, jsonify
 app = Flask(__name__, static_folder='./ui/build', static_url_path='/') # where static files are stored
 # static_url_path is seen in front end, static_folder seen in backend 
 
-# comment out on deployment
-#from flask_cors import CORS
-#CORS(app)
-
 if __name__ == 'main':
     app.run()
 
 # function to take input form frontend and test against "Sofie"
 @app.route("/input/<fname>", methods=["GET"])
 def input(fname: str):
-    print("fname=", fname)
     if fname == "Sofie":
         output = "Kardonik"
     else:
         output = "User Not Found"
-    print("out=", output)
     return jsonify(str=output)
 
 @app.route('/')
